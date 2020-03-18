689e9dd566f98104bf8e4bfa464b24735928be15
Jon Hill <jon@digitalstate.co.uk>
2020-03-18 21:16:44 +0000

    basic http function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
-import * as functions from 'firebase-functions';
+import * as express from 'express'
+import * as cors from 'cors'
+// import * as twilio from 'twilio'
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+const REGION = 'europe-west1'
+const app = express()
+app.use(cors({origin: true}))
+admin.initializeApp()
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+app.post('/', async (_req, resp) => {
+  console.log('app.post', resp)
+  resp.status(200)
+})
+exports.ashfield = functions.region(REGION).https.onRequest(app)
